d1cefc1e33d3087745cab376c91b978df01e993b
Kien NGUYEN <kennygunie@gmail.com>
2013-02-16 17:16:35 +0100

    Dynamic grid
    - new flip effect
    - dynamic grid with masonry
    - cocktail flash cards

--- js/main.js
+++ js/main.js
@@ -1,40 +1,62 @@
-function updateData(dataPath, numberPerRow, sort) {
-	$('#content .row-fluid').remove();
+function updateData(dataPath, sort) {
 	$.getJSON(dataPath, function(json) {
 	    //console.log(json[0].cards); // this will show the info it in firebug console
+	    
+	    var cards = json[0].cards;
 	    if (sort) {
-				json[0].cards.sort(function(x,y) { return ((x.front == y.front) ? 0 : ((x.front > y.front) ? 1 : -1 ));   } );
+				cards.sort(function(x,y) { return ((x.front == y.front) ? 0 : ((x.front > y.front) ? 1 : -1 ));   } );
 	    }
 	    
-	    var items = '<div class="row-fluid">';
-	    var size = 12/numberPerRow;
-	    $.each(json[0].cards, function(i, val) {
-	    	items += '<div class="flip-container span' + size + '"><div class="flipper"><div class="front"><div>' + val.front + '</div><div class="hint">' + val.hint + '</div></div><div class="back">'+ val.back + '</div></div></div>';    	
-	    	if((i+1)%numberPerRow == 0) {
-		    	items += '</div><div class="row-fluid">';
-	    	}
+	    
+	    var items = '<div id="flashcards">';
+	    var count;
+	    $.each(cards, function(i, val) {
+	    	items += '<div class="flip-container"><div class="flipper"><div class="front"><div>' + val.front + '</div><div class="hint">' + val.hint + '</div></div><div class="back">'+ val.back + '</div></div></div>';
+	    	count = i;	    	
 	    });
 	    items += '</div>';
+	    
 	    $('header h1').html(json[0].title);
-	    $('header p').html(json[0].subject);
-	    $('header').after(items);
+	    $('#description').html((count+1) + ' cards');
+	    
+	    $('#content').html(items);
+	    
+	    $('#flashcards').masonry({
+			// options
+			itemSelector : '.flip-container',
+			gutterWidth : 10,
+			isResizable: true,
+			isFitWidth: true,
+			isAnimated: true
+		});
+		
 	});
 }
 
-$('.flip-container').bind('touchstart', function(e){
-	this.classList.toggle('hover');
-});
-
-$('#km').click(function (e) {
-	updateData("data/km.json", 4, false);
-});
-
-$('#capital').click(function (e) {
-	updateData("data/capital.json", 6, true);
-});
-
-$('ul.nav > li').click(function (e) {
-	e.preventDefault();
-	$('ul.nav > li').removeClass('active');
-	$(this).addClass('active');                
+$(document).ready(function(){
+	$('.flip-container').bind('touchstart', function(e){
+		this.classList.toggle('hover');
+	});
+	
+	$('ul.nav > li').click(function (e) {
+		e.preventDefault();
+		$('ul.nav > li').removeClass('active');
+		$(this).addClass('active');                
+	});
+	
+	$('#km').click(function (e) {
+		updateData("data/km.json", false);
+	});
+	
+	$('#capital').click(function (e) {
+		updateData("data/capital.json", true);
+	});
+	
+	$('#java').click(function (e) {
+		updateData("data/java.json", false);
+	});
+	
+	$('#cocktail').click(function (e) {
+		updateData("data/cocktail.json", false);
+	});
 });
